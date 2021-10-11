083b188cc73defb532f898b6b9eb17e1a2550015
admin admin <evaladmin@example.com>
2021-10-11 17:09:13 +0700

    User admin wrote content /scripts/rest/contactUs.post.groovy

--- scripts/rest/contactUs.post.groovy
+++ scripts/rest/contactUs.post.groovy
@@ -5,11 +5,11 @@ println params
 
 def service = new MailService()
 // SMTP server information:
-service.setHost('smtp.gmail.com')
+service.setHost('pro50.emailserver.vn')
 service.setPort('587')
 service.setEncoding('utf-8')
 
-service.sendEmail("monet.phongvg@gmail.com", "it.keysoft@gmail.com", null, 
+service.sendEmail("contact@mavinfood.com", "online@mavinfood.com", null, 
                   "HauTK" ,"<strong>Họ Tên: </strong>" + "hau tk" + "<br/>" + "<strong>Email: </strong>" + "params.email" +"<br/>" + "<strong>Số điện thoại: </strong>" 
                   + "params.phone" + "<br/>" +"<strong>Tên công ty: </strong>" 
                   + "params.company" + "<br/>" + "<strong>Nội dung: </strong>" + "params.content")
